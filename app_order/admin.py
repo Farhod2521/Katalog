8418242f7aeefbf1c82906c06f9ecd02f191f4aa
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-04 11:35:23 +0500

    fixed

--- app_order/admin.py
+++ app_order/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from .models import Order, RatingCommentCustomer
 
 
-admin.site.register(Order)
+class CompanyFilter(admin.SimpleListFilter):
+    title = "Company"
+    parameter_name = "company"
+
+    def lookups(self, request, model_admin):
+        companies = Order.objects.values_list("company", flat=True).distinct()
+        return [(company, company) for company in companies]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(company=self.value())
+        return queryset
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer", "company", "product_name", "price", "quantity", "order_status", "create_at")
+    list_filter = (CompanyFilter, "order_status")
+
+    
 admin.site.register(RatingCommentCustomer)
 # Register your models here.
